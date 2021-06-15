eb45d6ad3fa38e94d95c6e0086c7b7f8988a497d
Pablo Waehner <waehnermp@gmail.com>
2021-06-15 15:59:27 +0200

    refactor home page to show status

--- src/main/java/com/mainapp/controllers/PageController.java
+++ src/main/java/com/mainapp/controllers/PageController.java
@@ -1,25 +1,31 @@
 package com.mainapp.controllers;
 
-import javax.validation.Valid;
-
-import org.springframework.data.domain.Page;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
 import com.mainapp.model.StatusUpdate;
+import com.mainapp.service.StatusUpdateService;
 
 @Controller //from model-view-controller. This class will figure out for each url what content should be served up to the browser
 public class PageController {
 	
-	@RequestMapping("/") // when the user goes to this url, that request will be handled by the home() function
-//	@ResponseBody // what we return in this function is what we want to send to the browser (we don't need it if we use jsp)
-	String home() {
-		return "app.homepage"; // renders the definition in the tiles.xml file
+//	@RequestMapping("/") // when the user goes to this url, that request will be handled by the home() function
+////	@ResponseBody // what we return in this function is what we want to send to the browser (we don't need it if we use jsp)
+//	String home() {
+//		return "app.homepage"; // renders the definition in the tiles.xml file
+//	}
+	
+	@Autowired
+	private StatusUpdateService statusUpdateService;
+	
+	@RequestMapping("/") 
+	ModelAndView home(ModelAndView modelAndView) {
+		StatusUpdate statusUpdate = statusUpdateService.getLatest();
+		modelAndView.getModel().put("statusUpdate", statusUpdate);
+		modelAndView.setViewName("app.homepage");
+		return modelAndView;
 	}
 
 	@RequestMapping("/about")
